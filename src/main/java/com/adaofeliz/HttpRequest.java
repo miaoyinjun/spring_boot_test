48ba287c1d48eb8bd8d5276eeb8147782f377daf
jimmy <miaoyinjun@gmail.com>
2017-02-27 15:28:53 +0800

    test

--- src/main/java/com/adaofeliz/HttpRequest.java
+++ src/main/java/com/adaofeliz/HttpRequest.java
@@ -12,6 +12,7 @@ import java.net.URLConnection;
 import java.util.List;
 import java.util.Map;
 
+
 public class HttpRequest {
     /**
      * 向指定URL发送GET方法的请求
@@ -44,17 +45,17 @@ public class HttpRequest {
             Map<String, List<String>> map = connection.getHeaderFields();
             // 遍历所有的响应头字段
             for (String key : map.keySet()) {
-                System.out.println(key + "--->" + map.get(key));
+                result += map.get(key);
             }
-            // 定义 BufferedReader输入流来读取URL的响应
-            in = new BufferedReader(new InputStreamReader(
-                    connection.getInputStream()));
-            String line;
-            while ((line = in.readLine()) != null) {
-                result += line;
-            }
+//            // 定义 BufferedReader输入流来读取URL的响应
+//            in = new BufferedReader(new InputStreamReader(
+//                    connection.getInputStream()));
+//            String line;
+//            while ((line = in.readLine()) != null) {
+//                result += line;
+//            }
         } catch (Exception e) {
-            result = e.getMessage();
+            result += e.getMessage();
             System.out.println("发送GET请求出现异常！" + e);
             e.printStackTrace();
         }
